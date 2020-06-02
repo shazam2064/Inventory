e347fc4ad86d1837c12272c1509adbbb6cf1f0bf
gabriel <gabriel.salomon.2007@gmail.com>
2020-06-02 06:45:17 -0400

    I forgot to change this too

--- src/main/java/com/gabo/inventory/constants/InventoryConstants.java
+++ src/main/java/com/gabo/inventory/constants/InventoryConstants.java
@@ -6,7 +6,7 @@ public class InventoryConstants {
 
     public static final String V1_PATH = "/v1";
     public static final String INVENTORY_V1_PATH = "/inventory" + V1_PATH;
-    public static final String WAREHOUSES_PATH = "/warehouses";
+    public static final String WAREHOUSE_PATH = "/warehouses";
     public static final String LOCATION_PATH = "/locations";
     public static final String GROUP_PATH = "/groups";
     public static final String UNIT_PATH = "/units";
